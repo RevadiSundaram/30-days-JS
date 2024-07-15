54e801051952f32b6bd51ddba9d037588e0571ad
revadisundaram7 <sundaramrevadi@gmail.com>
2024-07-15 23:38:51 +0530

    Committing Day 1-3 of js

---
+++ index.js
@@ -0,0 +1,131 @@
+// Day 1
+
+// var a = 10;
+// console.log(a);
+// console.log(typeof a);
+
+// let b="Hellooo World";
+// console.log(b);
+// console.log(typeof b);
+
+// b=20;
+// console.log(b);
+
+// const c=true;
+// console.log(c);
+// console.log(typeof c);
+// //c=false;
+// //console.log(c);
+
+// function type(par){
+//     console.log(par);
+//     console.log(typeof par);
+// }
+
+// type("Helloo");
+
+// Day 2
+
+// console.log("Day 2");
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// console.log(sum(10,5));
+
+// function sub(a,b){
+//     return a-b;
+// }
+
+// console.log(sub(10,5));
+
+// function mul(a,b){
+//     return a*b;
+// }
+
+// console.log(mul(10,5));
+
+// function div(a,b){
+//     return a/b;
+// }
+
+// console.log(div(10,5));
+
+// function mod(a,b){
+//     return a%b;
+// }
+
+// console.log(mod(10,3));
+
+
+// let x = 10;
+// x+=3;
+
+// console.log(x);
+
+// let x = 10;
+// x-=3;
+
+// console.log(x);
+
+// let a = 12;
+
+// let b = 9;
+
+// if ( (a || b) < 10){
+//     console.log("true");
+// }
+
+// function ter(x){
+//    return x>0 ? true : false
+// }
+// console.log(ter(-19));
+
+// Day 3
+
+// function ter(x){
+//    return x>0 || x==0 ? true : false
+// }
+// console.log(ter(0));
+
+// function eligible(x){
+//     return x >= 18 ? true : false
+// }
+// console.log(eligible(16));
+
+// let num;
+// switch(new Date().getDay()){
+
+//     case 0:
+//         num = "Sunday"
+//         break;
+//     case 1:
+//         num = "Monday"
+//         break;
+//     case 2:
+//         num="Tuesday";
+//         break;
+//     case 3:
+//         num="Wednesday"
+//         break;
+//     case 4:
+//         num="Thursday"
+//         break;
+//     case 5:
+//         num="Friday"
+//         break;
+//     case 6:
+//         num="Saturday"
+//         break;
+//     default: 
+//         num="Noday"
+
+// }
+// console.log(num);
+
+// function leap(x){
+//     return x%4 == 0 ? true : false;
+// }
+
+// console.log(leap(2023));
